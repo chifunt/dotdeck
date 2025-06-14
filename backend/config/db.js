caccffd5eb8062b75481b2f306936ddbfb9eecb9
Tjhie Joshua <cc241001@fhstp.ac.at>
2025-06-14 03:56:35 +0200

    refactor to parse db port and load dotenv just once

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,11 @@
  */
 
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 /** @type {mysql.PoolOptions} */
 const poolConfig = {
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
