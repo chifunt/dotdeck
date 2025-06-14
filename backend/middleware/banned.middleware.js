a516c37d4c21efd0667c6be220dd735301f09048
Tjhie Joshua <cc241001@fhstp.ac.at>
2025-06-14 03:09:32 +0200

    implement middleware to reject actions from banned users

---
+++ backend/middleware/banned.middleware.js
@@ -0,0 +1,25 @@
+/**
+ * @file Reject requests from permanently-banned users.
+ *
+ * Must be placed *after* authRequired so req.user exists.
+ */
+
+import { db } from "../config/db.js";
+
+/** @type {import('express').RequestHandler} */
+export async function bannedGuard(req, res, next) {
+  try {
+    const [[row]] = await db.query(
+      "SELECT banned_at FROM dotdeck_user WHERE id = ? LIMIT 1",
+      [req.user.id],
+    );
+
+    if (row?.banned_at) {
+      return res.status(403).json({ message: "Account banned" });
+    }
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
